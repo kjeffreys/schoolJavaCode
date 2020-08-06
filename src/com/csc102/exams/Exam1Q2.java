af3e58571335f9700c29d12c1c6ac83d40943165
kjeffreys <kajeffr1@asu.edu>
2020-08-05 21:44:37 -0400

    Exam1Q2.java created. Using method from Q1 in the same package until impl.

---
+++ src/com/csc102/exams/Exam1Q2.java
@@ -0,0 +1,8 @@
+package com.csc102.exams;
+
+public class Exam1Q2 {
+    public static void main(String[] args) {
+        //run program
+        System.out.println(Exam1Q1.firstQuestion());
+    }
+}
