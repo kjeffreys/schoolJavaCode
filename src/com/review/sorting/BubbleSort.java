c64d88821d060dba99ac6ed6ffac6e33a799f7cd
kjeffreys <kajeffr1@asu.edu>
2020-08-09 21:39:41 -0400

    BubbleSort.java

--- src/com/review/sorting/BubbleSort.java
+++ src/com/review/sorting/BubbleSort.java
@@ -1,4 +1,35 @@
 package com.review.sorting;
 
+import java.util.Arrays;
+
 public class BubbleSort {
+    public static void main(String[] args) {
+        int nums[] = {10,2,8,4,7,5,1,3};
+        printArray(nums);
+
+        //The easy way -- Do this at contests :D
+        //Arrays.sort(nums);
+
+        //Bubble sort
+        for(int x = 0; x < nums.length; x++) {
+            System.out.println("X is " + x);
+            for(int y = 0; y < nums.length - 1; ++y) {
+                if(nums[y] > nums[y+1]) {
+                    int temp = nums[y];
+                    nums[y] = nums[y+1];
+                    nums[y+1] = temp;
+                }
+                System.out.print(y + "    ");
+                printArray(nums);
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public static void printArray(int[] a) {
+        for(int x =0; x <a.length; x++) {
+            System.out.print(a[x] + " ");
+        }
+        System.out.println();
+    }
 }
